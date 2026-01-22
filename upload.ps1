119b14dcd8f7a2135779c5d81f026142d0c849f7
anwokeoma <anwokeoma@yahoo.com>
2026-01-22 18:53:33 +0100

    Automated push on 2026-01-22 18:53:33

--- upload.ps1
+++ upload.ps1
@@ -1,5 +1,5 @@
 # 1. Local Git Operations
-Write-Host "--- Moving into jb folder and starting Git ---" -ForegroundColor Cyan
+Write-Host "--- Moving into oa folder and starting Git ---" -ForegroundColor Cyan
 
 $Timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
 
@@ -12,10 +12,10 @@ Write-Host "--- Connecting to Remote Server ---" -ForegroundColor Cyan
 
 # Bundling Linux commands to execute over SSH
 $RemoteCommands = @"
-    cd jb && \
+    cd oa && \
     git pull && \
-    source /home/jbadmin/nodevenv/jb/24/bin/activate && \
-    cd /home/jbadmin/jb && \
+    source /home/adminoa/nodevenv/oa/10/bin/activate && \ 
+    cd /home/adminoa/oa && \
     npm cache clean --force && \
     npm install --production=false && \
     npm run build && \
@@ -24,6 +24,6 @@ $RemoteCommands = @"
 "@
 
 # Execute via SSH (It will prompt for password here)
-ssh jbadmin@jobiz.ng $RemoteCommands
+ssh adminoa@oneapp.ng $RemoteCommands
 
 Write-Host "--- All Done! ---" -ForegroundColor Green
